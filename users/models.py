bb8bd39020c983a13bab1bcf4997ee694ffe89f7
Ramon <ramin0703@gmail.com>
2024-06-07 17:30:14 +0330

    change back

--- users/models.py
+++ users/models.py
@@ -40,6 +40,3 @@ class User(Base):
         default=datetime.now(),
         nullable=False,
     )
-    # One-to-one relationship with Seat
-    # seat_id = db.Column(db.Integer, db.ForeignKey('seat.id'), nullable=True)
-    # seat = db.orm.relationship(back_populates="user")
