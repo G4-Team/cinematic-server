7cb4e838e5add337f53cadc4f9e7b54a8d0017e8
Ramon <ramin0703@gmail.com>
2024-06-10 02:33:25 +0330

    make cinema selector functions

--- cinema/selectors.py
+++ cinema/selectors.py
@@ -1,2 +1,18 @@
 from source.database import DatabaseConnection
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.query import Query
+from cinema.models import Cinema
+
+
+def get_cinema(id: id) -> Cinema:
+    with Session(DatabaseConnection.engin) as session:
+        query = session.query(Cinema)
+        cinema = query.get(id)
+    return cinema
+
+
+def filter_cinemas(**kwargs) -> Query:
+    with Session(DatabaseConnection.engin) as session:
+        query = session.query(Cinema)
+        cinemas = query.filter_by(**kwargs)
+    return cinemas
