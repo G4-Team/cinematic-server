cace056f7daf3d23298aa77baaf66adec3d14612
Ramon <ramin0703@gmail.com>
2024-06-10 23:11:20 +0330

    initialize add-showtime url

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,5 +4,6 @@ from cinema.views import *
 app_name = 'cinema'
 
 cinema_urls = {
-    "add-cinema/": add_cinema_view,
+    'add-cinema/': add_cinema_view,
+    'add-showtime/': add_showtime_view
 }
