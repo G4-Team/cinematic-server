0eea38e7002af25098c3c55f74128f6485078a9c
Ramon <ramin0703@gmail.com>
2024-06-11 01:41:14 +0330

    initialize remaining urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -8,4 +8,6 @@ cinema_urls = {
     'add-showtime/': add_showtime_view,
     'movie-showtimes/{movie_id}': list_movie_showtimes_view,
     'cinema-showtimes/{cinema_id}': list_cinema_showtimes_view,
+    'reserve/{user_id}/{showtime_id}': reserve_showtime_view,
+    'cancel/{user_id}/{showtime_id}': cancel_showtime_view
 }
